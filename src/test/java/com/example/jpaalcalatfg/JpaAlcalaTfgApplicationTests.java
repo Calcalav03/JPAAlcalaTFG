31963c7744be926e54db7f57dd11839bd2f20a76
Calcalav03MESA <carlos2003carlos@gmail.com>
2025-05-05 17:29:21 +0200

    Initial commit

---
+++ src/test/java/com/example/jpaalcalatfg/JpaAlcalaTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaalcalatfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAlcalaTfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
